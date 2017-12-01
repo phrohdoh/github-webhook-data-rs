a6b11a505938085b267f18dc55e2b14f3fb95bbf
Taryn Hill <taryn@phrohdoh.com>
2017-12-01 17:27:33 -0600

    Read server configuration from the command line

--- server/src/bin/main.rs
+++ server/src/bin/main.rs
@@ -1,7 +1,25 @@
 extern crate server as lib;
 
+extern crate structopt;
+use structopt::StructOpt;
+
+#[macro_use]
+extern crate structopt_derive;
+
+#[derive(StructOpt, Debug)]
+#[structopt(name = "server", about = "An HTTP server that accepts GitHub webhook API data (as JSON).")]
+struct CliOptions {
+    #[structopt(long = "port", help = "The port number to accept JSON at", default_value = "3000")]
+    port: u16,
+
+    #[structopt(long = "address", help = "The IP/URL/etc. to accept JSON at (not including the protocol)", default_value = "127.0.0.1")]
+    addr: String,
+}
+
 fn main() {
-    let addr = "127.0.0.1:3000";
-    println!("Running server on {}", addr);
-    lib::run(addr);
+    let opts = CliOptions::from_args();
+    let addr = format!("{}:{}", opts.addr, opts.port);
+
+    println!("Running server on {}", &addr);
+    lib::run(&addr);
 }
